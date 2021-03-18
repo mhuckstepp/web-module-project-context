5e0836d27d8ae6539cae417da59f38663cf1ed2a
Max Huckstepp <mhuckstepp@gmail.com>
2021-03-18 11:16:03 -0700

    stretch goals complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Route } from "react-router-dom";
 import data from "./data";
 import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
 
 // Components
 import Navigation from "./components/Navigation";
@@ -10,7 +11,7 @@ import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
   const [products] = useState(data);
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(data);
 
   const addItem = (item) => {
     setCart([...cart, item]);
@@ -18,18 +19,20 @@ function App() {
 
   return (
     <ProductContext.Provider value={{ products, addItem }}>
-      <div className="App">
-        <Navigation cart={cart} />
+      <CartContext.Provider value={{ cart, setCart }}>
+        <div className="App">
+          <Navigation cart={cart} />
 
-        {/* Routes */}
-        <Route exact path="/">
-          <Products />
-        </Route>
+          {/* Routes */}
+          <Route exact path="/">
+            <Products />
+          </Route>
 
-        <Route path="/cart">
-          <ShoppingCart cart={cart} />
-        </Route>
-      </div>
+          <Route path="/cart">
+            <ShoppingCart cart={cart} />
+          </Route>
+        </div>
+      </CartContext.Provider>
     </ProductContext.Provider>
   );
 }
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,27 +1,50 @@
-import React from 'react';
+import React from "react";
+import { useContext } from "react";
+import { CartContext } from "../contexts/CartContext";
 
 // Components
-import Item from './ShoppingCartItem';
+import Item from "./ShoppingCartItem";
 
-const ShoppingCart = props => {
-	const getCartTotal = () => {
-		return props.cart.reduce((acc, value) => {
-			return acc + value.price;
-		}, 0).toFixed(2);
-	};
+const ShoppingCart = () => {
+  const { cart, setCart } = useContext(CartContext);
 
-	return (
-		<div className="shopping-cart">
-			{props.cart.map(item => (
-				<Item key={item.id} {...item} />
-			))}
+  const removeHandler = (id) => {
+    console.log(id);
+    let res = cart.filter((book) => {
+      if (id !== book.id) {
+        console.log("hit if", id, book.id);
+        return book;
+      }
+    });
+    setCart(res);
+  };
 
-			<div className="shopping-cart__checkout">
-				<p>Total: ${getCartTotal()}</p>
-				<button>Checkout</button>
-			</div>
-		</div>
-	);
+  const getCartTotal = () => {
+    return cart
+      .reduce((acc, value) => {
+        return acc + value.price;
+      }, 0)
+      .toFixed(2);
+  };
+
+  return (
+    <div className="shopping-cart">
+      {cart.map((item) => {
+        return item ? (
+          <Item
+            key={item.id}
+            {...item}
+            removeHandler={() => removeHandler(item.id)}
+          />
+        ) : null;
+      })}
+
+      <div className="shopping-cart__checkout">
+        <p>Total: ${getCartTotal()}</p>
+        <button>Checkout</button>
+      </div>
+    </div>
+  );
 };
 
 export default ShoppingCart;
